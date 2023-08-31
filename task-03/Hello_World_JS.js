045d6476785943f107ed68677cc9690ce2e4e852
rihaan1810 <rihaan1810@gmail.com>
2023-08-31 23:33:51 +0530

    JavaScript

---
+++ task-03/Hello_World_JS.js
@@ -0,0 +1,13 @@
+var number = prompt();
+
+console.log(2);
+for (var i = 2; i <= number; i++) {
+    for (var j = 2; j < i; j++) {
+        if (i % j == 0) {
+            isPrime = false;
+            break;
+        }
+        console.log(i);
+        break;
+    }
+}
