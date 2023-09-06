64815e1fb543a70350a6066e73d07ae86e95deeb
rihaan1810 <rihaan1810@gmail.com>
2023-09-06 11:40:17 +0530

    Making a small change in js file

--- task-03/Hello_World_JS.js
+++ task-03/Hello_World_JS.js
@@ -1,13 +1,14 @@
 var number = prompt();
-
-console.log(2);
-for (var i = 2; i <= number; i++) {
-    for (var j = 2; j < i; j++) {
-        if (i % j == 0) {
-            isPrime = false;
+if(number > 1){
+    console.log(2);
+    for (var i = 2; i <= number; i++) {
+        for (var j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+            console.log(i);
             break;
         }
-        console.log(i);
-        break;
     }
 }
